57385de5cec12d76f58bf48ffba6f572f4bafb1a
sydrayyanasif <sydrayyanasif@gmail.com>
2025-10-29 18:06:11 +0530

    Commit

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
     session_start();
-    $conn = mysqli_connect('localhost', 'root', '', 'modern_bank');
+    $conn = mysqli_connect('mysql://rayyan:rayyan@modernbank-lob4v-mysql.modernbank-lob4v.svc.cluster.local:3306/modern_bank', 'rayyan', 'rayyan', 'modern_bank');
 ?>
